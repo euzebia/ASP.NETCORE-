5213aa211fd7a4aca67645c29e8bd7dff244a58f
euzebia <njeuzebia@gmail.com>
2021-08-04 12:33:54 +0300

    Webapi development using asp.net core 2.0

---
+++ Models/Customer.cs
@@ -0,0 +1,8 @@
+namespace MyFirstWebAPIProject.Models{
+    public class Customer{
+        public string FirstName { get; set; }
+        public string LastName  { get; set; }
+        public string Email  { get; set; }
+        public string PhoneNumber  { get; set; }
+    }
+}
